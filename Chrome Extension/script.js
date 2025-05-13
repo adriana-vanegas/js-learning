932d7b11b1bf9f5185dea4537b70b6f1c6177f60
Adriana <139722008+adriana-vanegas@users.noreply.github.com>
2025-05-12 22:49:15 -0400

    update

--- Chrome Extension/script.js
+++ Chrome Extension/script.js
@@ -1,7 +1,7 @@
 const saveBtn = document.getElementById("save-btn");
-const showBtn = document.getElementById("output-links");
 const leadInput = document.getElementById("input");
 const output = document.getElementById("outputLinks");
+const deleteBtn = document.getElementById("delete-btn");
 let listLinks = "";
 
 let existingLeads = localStorage.getItem("myLeads");
@@ -13,26 +13,32 @@ saveBtn.addEventListener("click", function () {
       // if there are existing leads, have it start with the existing leads
       jsLeads = JSON.parse(existingLeads);
     }
-
     jsLeads.push(leadInput.value); // add the most recent input value to the list
     localStorage.setItem("myLeads", JSON.stringify(jsLeads)); // set the list into a string and upload it to local storage
     existingLeads = localStorage.getItem("myLeads"); // update the existing leads as a string
-    renderLeads();
+    render(jsLeads);
     leadInput.value = ""; // null out the value so it can store the next one
   } else leadInput.reportValidity();
 });
 
-function renderLeads() {
+function render(leads) {
   listLinks = "";
-  for (i = 0; i < jsLeads.length; i++) {
+  for (i = 0; i < leads.length; i++) {
     listLinks += `
     <li>
-      <a target="_blank" href="${jsLeads[i]}">
-      ${jsLeads[i]}</a>
+      <a target="_blank" href="${leads[i]}">
+      ${leads[i]}</a>
     </li>`;
   }
   output.innerHTML = listLinks;
 }
+
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  existingLeads = "";
+  jsLeads = [];
+  render(jsLeads);
+});
 
 // Another way to do it with create element
 // showBtn.addEventListener("click", function () {

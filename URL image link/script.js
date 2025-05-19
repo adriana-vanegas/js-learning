5222dd0c77411c10f8f9dbb1f2a5e86df0f865e7
Adriana <139722008+adriana-vanegas@users.noreply.github.com>
2025-05-19 14:30:51 -0600

    practice links

---
+++ URL image link/script.js
@@ -0,0 +1,20 @@
+link = [
+  "https://drive.google.com/drive/u/0/home",
+  "https://docs.google.com/spreadsheets/d/1KoNy7j9SK-V4Ywue_THNH32-Wi5ffAq83dFBZMcbxxI/edit?gid=0#gid=0",
+  "https://docs.google.com/document/d/1VtndYUhFVAV2YgFoFv8BkqKc46oWh34Rq-PFzuZnPHU/edit?tab=t.0",
+  "https://www.figma.com/design/JR00zSKlUSco4P74nNF5uB/Untitled?node-id=26-997&p=f&t=V22xqWCKbDdXydiG-0",
+  "https://github.com/adriana-vanegas",
+  "https://chatgpt.com/c/6812e797-d6c4-8011-9377-c873783be2a5",
+  "https://airtable.com/login",
+  "https://www.notion.so/login",
+  "https://id.atlassian.com/login?continue=https%3A%2F%2Fwww.atlassian.com%2Fgateway%2Fapi%2Fstart%2Fauthredirect",
+  "https://auth.monday.com/p/software/users/sign_up_new?origin=hp_fullbg_page_header&_gl=1*1i2149v*_up*MQ..*_gs*MQ..&gclid=Cj0KCQjwiqbBBhCAARIsAJSfZkac-bFilXBFhOuwkE1_ygblAErO6nVhmNsAtDkHZPUAVHuWlN165W0aAoX1EALw_wcB&gbraid=0AAAAADeiQJuE360BmveBMLUp8pYngxMx3",
+  
+];
+
+console.log(new URL(link).pathname);
+
+// const imageMap = {
+//   'docs.google.com': ,
+
+// }

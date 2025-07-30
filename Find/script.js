b4da6e0b35000c51cb4b306015f508296773f545
Adriana <139722008+adriana-vanegas@users.noreply.github.com>
2025-07-30 13:22:21 -0400

    Quick practice
    Map, Reduce, Filter

--- Find/script.js
+++ Find/script.js
@@ -24,3 +24,28 @@ if (duplicateName) {
 } else {
   console.log("All set");
 }
+
+// How to use Map
+const listMap = list.map((item) => item.name);
+console.log(listMap);
+
+// How to use Reduce
+const prices = [85, 90, 95, 65];
+
+// In reduce, the returned value becomes the accumulator
+const total = prices.reduce((accumulator, next) => accumulator + next);
+console.log(total);
+// SUM:
+// First iteration: acc: 85, next: 90 -> 175
+// Second iteration: acc: 175, next: 95 -> 270
+// Third iteration: acc: 270, next: 65 -> 335
+
+const max = prices.reduce((accumulator, next) => Math.max(accumulator, next));
+console.log(max);
+// MAX:
+// First iteration: acc: 85, next: 90 -> 90
+// Second iteration: acc: 90, next: 95 -> 95
+// Third iteration: acc: 95, next: 65 -> 95
+
+const filtered = list.filter((item) => item.name != "Adriana");
+console.log(filtered);

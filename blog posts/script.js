7bc8e518b042c97dcb44808a3efcd6d7d9c4add6
Adriana <139722008+adriana-vanegas@users.noreply.github.com>
2025-08-07 22:41:09 -0400

    add form

--- blog posts/script.js
+++ blog posts/script.js
@@ -11,13 +11,23 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
       (item) =>
         (html += `<div class="post" data-id = ${item.id}>
                <h2>${item.title}</h2>
-               <p>${item.userID}</p>
+               <p>${item.userId}</p>
                <p>${item.body}</p>
                </div>`)
     );
 
     postSection.innerHTML = html;
-    console.log(html);
+    // console.log(html);
 
-    console.log(first5);
+    // console.log(first5);
   });
+
+const blogForm = document.getElementById("blog-submit");
+
+blogForm.addEventListener("click", function (e) {
+  e.preventDefault();
+  const titleInput = document.getElementById("title-input");
+  const bodyInput = document.getElementById("body-input");
+  console.log(titleInput.value);
+  console.log(bodyInput.value);
+});
